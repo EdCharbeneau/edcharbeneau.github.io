f39181b80335ccc3f436ceaa82524fa5fa7d098b
Ed Charbeneau <ed.charbeneau@gmail.com>
2018-03-19 21:07:34 -0400

    updated date format

--- javascript/session-list.js
+++ javascript/session-list.js
@@ -1,15 +1,18 @@
-              
+
 function mapScheduleToTemplate(schedule, itemTemplate, filterBy) {
-			return	schedule.filter(filterBy)
-                .sort((a,b) => new Date(a.date) - new Date(b.date))
-                .map(i => itemTemplate(i)).join('');
-}                
-  function templateLiteral(sch) {
-    return `<li class='schedule-item'>
+  return schedule.filter(filterBy)
+    .sort((a, b) => new Date(a.date) - new Date(b.date))
+    .map(i => itemTemplate(i)).join('');
+}
+
+const dateFormat = { month: 'short', day: 'numeric' };
+
+function templateLiteral(sch) {
+  return `<li class='schedule-item'>
               <div class='row row-flex'>
               <div class='col-md-4'>
                   <div class='schedule-item-box'>
-                   <span class='schedule-date'>${new Date(sch.date).toLocaleDateString("en-US",{  month: '2-digit', day: 'numeric' })}</span>
+                   <span class='schedule-date'>${new Date(sch.date).toLocaleDateString("en-US", dateFormat)}</span>
                    <span class='schedule-location'>${sch.location}</span>
                   </div>
                 </div>
@@ -21,4 +24,4 @@ function mapScheduleToTemplate(schedule, itemTemplate, filterBy) {
                 </div>
               </div>
             </li>`;
-  }
+}
