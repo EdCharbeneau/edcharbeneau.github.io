29d85e1057cb8d588f7a25fdcfa2b240f526bca1
Ed Charbeneau <ed.charbeneau@gmail.com>
2024-12-09 15:12:00 -0500

    Update webllm-interop.js

--- javascript/webllm-interop.js
+++ javascript/webllm-interop.js
@@ -1,34 +1,25 @@
 import * as webllm from "https://esm.run/@mlc-ai/web-llm";
 
-var dotnetInstance;
-var engine;
+var engine; // <-- hold a reference to MLCEngine in the module
+var dotnetInstance; // <-- hold a reference to the WebLLMService instance in the module
 
 // Callback function to update model loading progress
 const initProgressCallback = (initProgress) => {
-	//console.log(initProgress);
-	dotnetInstance.invokeMethodAsync("OnInitializing", initProgress);
+    // console.log(initProgress);
+    // Make a call to.NET with the updated status
+    dotnetInstance.invokeMethodAsync("OnInitializing", initProgress);
 }
 
 export async function initialize(selectedModel, dotnet) {
-	dotnetInstance = dotnet;
-	console.log(dotnet);
-	//console.log("initialize start");
-	engine = await webllm.CreateMLCEngine(
-		selectedModel,
-		{ initProgressCallback: initProgressCallback }, // engineConfig
-	);
+    dotnetInstance = dotnet; // <-- WebLLMService insntance
+    //console.log("initialize start");
+    engine = await webllm.CreateMLCEngine(
+        selectedModel,
+        { initProgressCallback: initProgressCallback }, // engineConfig
+    );
 }
 
-//export async function complete(messages) {
-//	console.log(messages);
-//	var reply = await engine.chat.completions.create({
-//		messages,
-//	});
-//	console.log(reply);
-//	return reply;
-//}
-
-export async function completeStream(messages, dotnetReceiver) {
+export async function completeStream(messages) {
 	// Chunks is an AsyncGenerator object
 	const chunks = await engine.chat.completions.create({
 		messages,
@@ -39,6 +30,7 @@ export async function completeStream(messages, dotnetReceiver) {
 
 	for await (const chunk of chunks) {
 		//console.log(chunk);
-		await dotnetReceiver.invokeMethodAsync("ReceiveChunkCompletion", chunk);
+		await dotnetInstance.invokeMethodAsync("ReceiveChunkCompletion", chunk);
 	}
 }
+
